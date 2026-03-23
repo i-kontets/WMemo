63d1965dba53e56a66acfbaa94a622acd7e4553a
ono <2410019@i-seifu.jp>
2026-03-23 16:47:02 +0900

    app変更

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import Header from "./components/layout/Header";
+import SearchBar from "./components/ui/SearchBar";
+import MemoList from "./components/memo/MemoList";
+import AddButton from "./components/ui/AddButton";
 
 function App() {
   return (
     <div>
       <Header />
+      <SearchBar />
+      <MemoList />
+      <AddButton />
     </div>
   );
 }
