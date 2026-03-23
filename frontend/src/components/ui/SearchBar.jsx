0a4bfa9ea095e773029b99e4ff91ed40319c0e9e
ono <2410019@i-seifu.jp>
2026-03-23 16:45:36 +0900

    検索ボックス試作

---
+++ frontend/src/components/ui/SearchBar.jsx
@@ -0,0 +1,11 @@
+function SearchBar() {
+  return (
+    <input
+      type="text"
+      placeholder="検索..."
+      style={{ width: "100%", padding: "8px" }}
+    />
+  );
+}
+
+export default SearchBar;
